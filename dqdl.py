ad9c7892728a17619c4c68caefb120a980aa2e88
Eugene Kim <ek@karma3labs.com>
2024-08-10 13:32:06 -0700

    feat: add --log-level

--- dqdl.py
+++ dqdl.py
@@ -13,20 +13,34 @@ from dune_client.client import DuneClient
 
 _logger = logging.getLogger(__name__)
 
+log_levels = {n.lower(): v
+              for n, v in logging.getLevelNamesMapping().items()
+              if n not in ('NOTSET', 'WARN')}
+
 
 class Format(StrEnum):
     CSV = auto()
     PQT = auto()
 
 
+def log_level(name: str) -> int:
+    try:
+        return log_levels[name]
+    except KeyError as e:
+        raise ValueError from e
+
+
 def main():
-    logging.basicConfig(level=logging.WARNING)
+    logging.basicConfig()
     parser = argparse.ArgumentParser()
     defaults = dict(
         format=Format.CSV,
         api_key='env:DUNE_API_KEY',
         dotenv=Path('.env'),
     )
+    log_level_names = [kv[0]
+                       for kv in sorted(log_levels.items(),
+                                        key=lambda kv: kv[1])]
     parser.set_defaults(**defaults)
     parser.add_argument('-o', '--output', metavar='FILE', type=Path,
                         help=f"""output filename
@@ -44,15 +58,23 @@ def main():
     parser.add_argument('-E', '--no-dotenv', dest='dotenv',
                         action='store_const', const=None,
                         help=f"""disable dotenv processing""")
+    parser.add_argument('--log-level', metavar='LEVEL',
+                        type=log_level,
+                        help=f"""enable log messages at LEVEL or higher; 
+                                 one of: {', '.join(log_level_names)}""")
     parser.add_argument('query', metavar='QUERY', type=int,
                         help=f"""Dune query number""")
     args = parser.parse_args()
+    if args.log_level is not None:
+        _logger.setLevel(args.log_level)
     if args.dotenv is not None:
         dotenv.load_dotenv(args.dotenv)
     output = args.output or f'{args.query}.{args.format}'
     with passarg.reader() as read_pass:
         api_key = read_pass(args.api_key)
     client = DuneClient(api_key=api_key)
+    if args.log_level is not None:
+        client.logger.setLevel(args.log_level)
     df: pd.DataFrame = client.get_latest_result_dataframe(args.query)
     if output == '-':
         output = sys.stdout
